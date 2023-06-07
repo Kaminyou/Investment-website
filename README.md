dbc6197b63fbb7a4f0e2d69a4039d7ece4644785
Kaminyou <ikaminyou@gmail.com>
2023-06-07 16:56:52 +0800

    improve structure

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 Investment website
 
 ## Production mode
+1. Please create `.env` and configurate it as:
+```env
+SQLALCHEMY_DATABASE_URI=...
+JWT_SECRET_KEY=...
+ADMIN_ACCOUNT=..
+```
+2. Please modify `backend/sql/create_user.sql` to create root user
+3. Server up
 ```bash
 $ docker-compose -f docker-compose.yml up --build -d
 ```
--- frontend/src/container/main.container.js
+++ frontend/src/container/main.container.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import config from "../config.json";
+import GitHubButton from 'react-github-btn'
+
+
 function MainPage({ token }) {
   return (
     <div className="main-wrapper">
       <div className="main-inner">
         <h2>SPARK investment website</h2>
-        <p><i>{config.VERSION}</i></p>
+        <p><i>{config.VERSION} created by <a href="https://github.com/Kaminyou">Ming-Yang Ho</a></i></p>
+        <GitHubButton href="https://github.com/Kaminyou/Investment-website" aria-label="Star Kaminyou/Investment-website on GitHub">Star</GitHubButton>
         <h5>Invest your favorite group</h5>
         <p>Please follow the steps to send request</p>
         {!token && token!=="" &&token!== undefined? (
